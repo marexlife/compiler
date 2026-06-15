2c1e28b03610795bb23bdfbe69dba4962bc8ca4b
marcel <marcel.hajek7@gmail.com>
2026-06-16 00:08:15 +0200

    added lex and updated clang-tidy

---
+++ src/lex/lex.h
@@ -0,0 +1,10 @@
+#ifndef COMPILER_LEX_LEX_H_
+#define COMPILER_LEX_LEX_H_
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace compiler::lexer {
+[[nodiscard]] std::vector<std::string> Lex(std::string_view source_text);
+}
+#endif  // COMPILER_LEXER_LEXER_H_
