043611a9c0fa648ca199ab36dca6c3c2eb593229
marcel <marcel.hajek7@gmail.com>
2026-06-16 02:18:15 +0200

    added CharTable

--- src/lex/lex.cc
+++ src/lex/lex.cc
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include "detail/char_table.h"
+
 namespace compiler::lex {
 [[nodiscard]] auto Lex(const std::string_view source_text)
     -> std::vector<std::string> {
@@ -15,6 +17,15 @@ namespace compiler::lex {
   }
 
   for (const auto source_text_char : source_text) {
+    switch (source_text_char) {
+      case detail::CharTable::kSpace:
+        [[fallthrough]];
+      case detail::CharTable::kNewLine:
+        
+        break;
+      default:
+        break;
+    }
   }
 
   return result;
