957d19efd657ed5a86e2e3ea303b9558b698bd9d
marcel <marcel.hajek7@gmail.com>
2026-08-04 23:21:05 +0200

    added Print node and fixed Token -> Node mismatch

---
+++ src/parse/nodes/print_node.h
@@ -0,0 +1,10 @@
+#ifndef COMPILER_PARSE_NODES_PRINT_NODE_H_
+#define COMPILER_PARSE_NODES_PRINT_NODE_H_
+#include "singal_node.h"
+
+namespace compiler::parse::nodes {
+class PrintNode final : public SingalNode {
+ public:
+};
+}  // namespace compiler::parse::nodes
+#endif  // COMPILER_PARSE_NODES_PRINT_NODE_H_
