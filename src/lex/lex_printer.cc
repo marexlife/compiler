19a66734f9397cd8d4ea30bc24dd749e5a5317d5
marcel <marcel.hajek7@gmail.com>
2026-08-07 01:56:00 +0200

    improved LexPrinter

--- src/lex/lex_printer.cc
+++ src/lex/lex_printer.cc
@@ -7,10 +7,10 @@
 namespace compiler::lex {
 void LexPrinter::PrintLexerResult(lex::TokenStream& token_stream) {
   for (lex::Statement statement : token_stream) {
-    spdlog::info("Statement: ");
+    spdlog::info("Statement:\n");
 
     for (lex::Token& token : statement) {
-      spdlog::info(std::format("Token: {}", token.lexeme()));
+      spdlog::info(std::format("  Token: {}", token.lexeme()));
     }
   }
 
