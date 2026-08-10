de89cdb460a193a1525057c7746af5b706dd553a
marcel <marcel.hajek7@gmail.com>
2026-08-10 04:17:07 +0200

    prints err more directly now

--- src/core/Logger.cpp
+++ src/core/Logger.cpp
@@ -1,12 +1,9 @@
 #include "Logger.h"
 #include "spdlog/spdlog.h"
-#include <format>
 
 namespace compiler::core {
 void Logger::log(absl::Status error)
 {
-    const auto formatedMesaage = std::format("{}", error.message());
-
-    spdlog::error(formatedMesaage);
+    spdlog::error(error.message());
 }
 }
