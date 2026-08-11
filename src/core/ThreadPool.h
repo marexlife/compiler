2cf90d80f968ce9b29b2f47d39617f6d11f54fce
marcel <marcel.hajek7@gmail.com>
2026-08-11 05:24:41 +0200

    added ThreadPool

---
+++ src/core/ThreadPool.h
@@ -0,0 +1,8 @@
+#ifndef COMPILER_CORE_THREADPOOL_H
+#define COMPILER_CORE_THREADPOOL_H
+namespace compiler::core {
+class ThreadPool final {
+  public:
+};
+} // namespace compiler::core
+#endif // COMPILER_CORE_THREADPOOL_H
