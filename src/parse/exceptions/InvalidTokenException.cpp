59c65b2ec90dd0be5fe28ab142d5d777c7d206ec
marcel <marcel.hajek7@gmail.com>
2026-08-25 14:12:12 -0400

    added what in InvalidTokenException.cpp

--- src/parse/exceptions/InvalidTokenException.cpp
+++ src/parse/exceptions/InvalidTokenException.cpp
@@ -1,4 +1,8 @@
 #include "InvalidTokenException.h"
 
 namespace marex::parse::exceptions {
+const char* InvalidTokenException::what()
+    const noexcept {
+    return full_message.data();
+}
 }  // namespace marex::parse::exceptions
