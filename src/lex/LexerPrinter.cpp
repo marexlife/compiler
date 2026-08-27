94ace8990c49cc89b74bdba460396d9ed584a8f0
marcel <marcel.hajek7@gmail.com>
2026-08-27 14:12:36 +0200

    added Lexer print start and lexer print end

--- src/lex/LexerPrinter.cpp
+++ src/lex/LexerPrinter.cpp
@@ -1,17 +1,20 @@
 #include "LexerPrinter.h"
 
+#include <iostream>
+
 #include "Token.h"
 #include "TokenStream.h"
 
 namespace marex::lex {
 void LexerPrinter::print_token_stream(
-    lex::TokenStream& token_stream)
-{
+    lex::TokenStream& token_stream) {
+    std::cout << "Lexer print start\n";
     for (lex::Token& token : token_stream) {
         std::cout << "  Token: " << token.get_lexeme()
                   << '\n';
     }
 
+    std::cout << "Lexer print end\n";
     std::cout.flush();
 }
-} // namespace marex::lex
+}  // namespace marex::lex
