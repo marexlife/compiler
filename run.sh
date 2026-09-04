d2b8b915f5168f71f771a63a5190b37fa94508ca
= <=>
2026-09-04 22:48:27 +0200

    walker now does not try to compile and exec

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@ cmake . -B build -GNinja
 cmake --build build
 cd tests
 ./../build/src/main/main main.mrx
+clang main.c -o app
+./app
--- src/walk/Walker.cpp
+++ src/walk/Walker.cpp
@@ -21,32 +21,5 @@ void Walker::run(
 
     core::Logger::log_info(
         std::format("wrote {}", output_file));
-
-#ifdef _WINT32
-    static std::string_view app_name = "app.exe";
-#else
-    static std::string_view app_name = "app";
-#endif
-
-    auto compile_command = std::format(
-        "gcc {} -o {}", output_file, app_name);
-    auto compile_command_result =
-        std::system(compile_command.data());
-
-    if (compile_command_result != 0) {
-        throw std::runtime_error(
-            "compiling file failed");
-    }
-
-    auto current_path =
-        std::filesystem::current_path() / app_name;
-
-    auto run_command_result =
-        std::system(current_path.c_str());
-
-    if (run_command_result != 0) {
-        throw std::runtime_error(
-            "compiling app failed");
-    }
 }
 }  // namespace marex::walk
